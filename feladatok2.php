e00de373a4ccd9dceff3a6a7641f62227844da08
András Polák <166911488+PolakAndras@users.noreply.github.com>
2025-11-13 15:12:08 +0000

    Második körös feladatok kész

--- feladatok2.php
+++ feladatok2.php
@@ -149,13 +149,13 @@ $diakok = [
 <form method="post">
   <label>Add meg a pontszámot ami felett átment a diák:</label> 
   <input type="number" name="megadottPontszam"> 
-  <button type="submit" name="atment">Küld</button>
+  <button type="submit" name="atmentkuldes">Küld</button>
 </form>
 
 <?php
 $atmentdiak = [];
 
-if ( isset( $_POST['atment'] ) ) {
+if ( isset( $_POST['atmentkuldes'] ) ) {
 foreach($diakok as $diak => $pontszam) {
 
     if( $pontszam >= $_POST['megadottPontszam'] ) {
@@ -175,12 +175,132 @@ foreach($diakok as $diak => $pontszam) {
     } else {
         print("Senki sem ment át.");
     }
-
-
 }
+?>
+<hr>
+<p>4. feladat – Tömbből HTML táblázat generálása <br>
+Cél: Tömb → HTML kimenet generálás. <br>
+Használat: <br>
+Ezt minden olyan projekt használja, ami adatokat listáz táblázatos formában: adminfelület, jelentések, statisztikák, ügyféladatok.
+</p> 
+<p>Készíts egy asszociatív tömböt termékekkel (nev, ar, keszleten), majd generálj belőle HTML table-t.</p>
+
+<?php
 
 
+$termekek = [
+  [
+    "nev" => "Alma",
+    "ar" => 250,
+    "keszleten" => true
+  ],
+  [
+    "nev" => "Banán",
+    "ar" => 300,
+    "keszleten" => false
+  ],
+  [
+    "nev" => "Körte",
+    "ar" => 400,
+    "keszleten" => true
+  ]
+  ];
 
 ?>
+<form method="post"><button name="tableGenerateB" type="submit">Küld</button></form>
+
+<?php
+if (isset( $_POST['tableGenerateB'] ) ){
+echo "<hr>";
+echo "<table style='border:1px solid black; width:100%'>";
+echo "<tr>";
+echo "<th style='border:1px solid black'>Név</th>";
+echo "<th style='border:1px solid black'>Ár</th>";
+echo "<th style='border:1px solid black; width:20%'>Készeleten</th>";
+echo "</tr>";
+
+
+    foreach($termekek as $termek) {
+        
+        echo "<tr>";
+        print("<td style='border:1px solid black'>" .  $termek["nev"] . "</td>" . 
+        "<td style='border:1px solid black'>" .  $termek["ar"] . "</td>"); 
+
+        if($termek['keszleten']){
+            print("<td style='border:1px solid black'> Van termék készeleten </td>");
+        } else {
+            print("<td style='border:1px solid black'> Nincs termék készeleten </td>");
+        }
+        echo "</tr>";
+    };
+    echo "</table>";
+    echo "<hr><hr>";
+};
+?>
+
+<hr>
+<p>5.feladat Egyszerű szűrőlista (GET)
+
+Cél: Szűrés URL-paraméter alapján.
+Használat:
+Keresések, kategória szűrők, oldalszámok – minden dinamikus webhely alapja.
+Ezt használják blogok, webshopok, híroldalak szűrőinél.</p>
+<p>Feladat: Legyen egy tömb 3-4 „cikkel”, mindegyikhez kategória tartozzon.
+Ha a felhasználó kiválaszt egy kategóriát, csak azokat jelenítsd meg.</p>
+
+
+
+
+
+
+<form method="get">
+    <label>Kategória:
+      <select name="kategoriak">
+        <option value="osszes">Összes</option>
+        <option value="etetes">Etetés</option>
+        <option value="nyultartas">Nyúltartás</option>
+        <option value="betegseg">Betegségek</option>
+      </select>
+    </label>
+    <button type="submit" name="elkuldve5">Szűrés</button>
+  </form>
+
+  <?php
+
+  $cikkek = [
+    "osszes" => [
+        "etetes" => ["e1", "e2", "e3"],
+        "nyultartas" => [ "t1", "t2", "t3", "t4"],
+        "betegsegek" => ["b1", "b2", "b3"],
+    ],
+];
+if(isset($_GET['elkuldve5']) ) {
+    //print($_GET['kategoriak']);
+    echo "<ul>";
+    print("A választott kategória: " . $_GET['kategoriak']);
+    
+    if ($_GET['kategoriak'] == "osszes") {
+        foreach($cikkek['osszes']['etetes'] as $oecikk) {
+            print("<li>" . $oecikk . "</li>");
+            }
+        foreach($cikkek['osszes']['nyultartas'] as $onycikk) {
+            print("<li>" . $onycikk . "</li>");
+            }
+        foreach($cikkek['osszes']['betegsegek'] as $obcikk) {
+            print("<li>" . $obcikk . "</li>");
+            }
+    }else {
+        foreach($cikkek['osszes'][$_GET['kategoriak']] as $cikk) {
+            print("<li>" . $cikk . "</li>");
+        }
+ echo "</ul>";
+    }
+}
+//print_r($cikkek['osszes']['etetes'])
+
+
+  ?>
+
+
 </body>
 </html>
